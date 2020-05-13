ed06af6f2f30af0f5f40bef3647da68f8077581a
Mustafa <mcaliskan90@gmail.com>
2020-05-13 13:25:01 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,30 +5,30 @@ import datetime
 import array
 
 
-bot_token ='964279669:AAEYDJUP0E6dWroXd01Uq6shqpClggNc-z0'
+bot_token ='token'
 bot =telebot.TeleBot(token=bot_token)
 
 an = datetime.datetime.now()
 tarih = datetime.datetime.ctime(an)
 
-kisiler = ["Hazal", "yetis", "mustafa"]
+kisiler = ["hak", "yetis", "mustafa"]
 
-#for i in range(len(kisiler)):
- #   @bot.message_handler(commands=kisiler[i])
-  #  def send_welcome(message):
-   #     bot.reply_to(message,'Giris Saati: ' + tarih)
+for i in range(len(personal)):
+    @bot.message_handler(commands=kisiler[i])
+    def send_welcome(message):
+        bot.reply_to(message,'Login:tarih)
 
 
-@bot.message_handler(commands=kisiler[0])
+@bot.message_handler(commands=personal[0])
 def send_welcome(message):
-    bot.reply_to(message, kisiler[0]+ ': Giris Saati: ' + tarih)
+    bot.reply_to(message, personal[0]+ ': Login + tarih)
 
-#@bot.message_handler(commands=kisiler[1])
-#def send_welcome1(message1):
-#    bot.reply_to(message1, 'Adnan Hep Bırçi')
-#    
-#@bot.message_handler(commands=kisiler[2])
-#def send_welcome2(message2):
-#    bot.reply_to(message2, 'Patron Geldi')
+@bot.message_handler(commands=personal[1])
+def send_welcome1(message1):
+    bot.reply_to(message1, 'test message
+    
+@bot.message_handler(commands=personal[2])
+def send_welcome2(message2):
+    bot.reply_to(message2, 'Bos )
 
 bot.polling()
